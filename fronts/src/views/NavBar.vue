1b3a703e966abfd113719fc41c8780eb3d4dbdcc
Acrazie <mayeul.desbazeille@epitech.eu>
2024-04-08 11:23:37 +0200

    [ADD] Log In Button

--- fronts/src/views/NavBar.vue
+++ fronts/src/views/NavBar.vue
@@ -19,11 +19,17 @@
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+
 .navbar{
     display: flex;
     justify-content: space-between;
     align-items:center;
     background-color: #B8B8B8;
+    /* font-family: 'Poppins', sans-serif; */
+    margin-bottom: 20px;
+    
 }
 .title{
     display: flex;
@@ -49,13 +55,20 @@
     margin: 5px 10px;
     border: none;
     border-radius: 5px;
+    font-family: 'Poppins', sans-serif;
     
 }
 .search {
     width: 300px;
+    transition: 0.2s;
 }
+.search:hover{
+    border: solid #FF4500;
+}
+
 .search-btn{
     background-color: white;
+    transition: 0.3s;
 }
 .search-btn:hover {
     background-color: #FF4500;
--- fronts/src/views/SubReppit.vue
+++ fronts/src/views/SubReppit.vue
@@ -3,41 +3,67 @@
 </script>
 
 <template>
-    <div class="Menu">
-        <h2 class="HeaderSubReppit">SubReppit</h2>
-            <ul class="divsubreppit">
-                <li class="subreppit">Gaming</li>
-                <li class="subreppit">Funny</li>
-                <li class="subreppit">Series</li>
-            </ul>
+    <div class="leftmenu">
+        <div class="login">
+                <a href="" class="btnlogin">Log In</a>
+        </div>
+        <div class="Menu">
+                <ul class="divsubreppit">
+                    <li class="subreppit">Gaming</li>
+                    <li class="subreppit">Funny</li>
+                    <li class="subreppit">Series</li>
+                </ul>
+        </div>
     </div>
 </template>
 
 <style scoped>
-body{
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+*{
     box-sizing: border-box;
     margin: 0;
     padding: 0;
 }
-.Menu{
-    width: 200px;
 
+.leftmenu{
+    width: 200px;
 }
-.HeaderSubReppit{
+.login{
     display: flex;
     justify-content: center;
-    overflow-wrap: break-word;
-    margin: 5px 10px;
+    align-items: center;
+    margin-bottom: 20px;
+    text-align: center;
 }
+
+.btnlogin{
+    text-decoration: none;
+    list-style-type: none;
+    background-color: #FF4500;
+    border: solid 1px #808080;
+    border-radius: 5px;
+    color: white;
+    width: 60%;
+    padding: 5px 10px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+}
+
+.btnlogin:hover{
+    background-color: #ee3f00;
+}
+
 .divsubreppit{
     display: table;
     margin: 0 auto;
-    /* width: fit-content; */
+    width: 80%;
     padding: 0;
     background-color: #FAFAFA;
     border: solid 1px #808080;
     border-radius: 5px;
-    box-shadow: -3px 3px 15px -2px rgba(0,0,0,0.78)
+    box-shadow: 0px 0px 5px 0.2px rgba(64, 64, 64, 0.78)
+    
 }
 .subreppit{
     text-decoration: none;
@@ -45,13 +71,16 @@ body{
     text-align: center;
     margin: 5px 10px;
     padding: 5px 10px;
-    background-color: #FF4500;
+    background-color: #B8B8B8;
     border: solid 1px #808080;
     border-radius: 5px;
     color: white;
+    font-family: 'Poppins', sans-serif;
+    transition: 0.3s;
 }
+
 .subreppit:hover{
-    background-color: #808080;
+    background-color: #FF4500;
     color: #FAFAFA;
     cursor: pointer;
 }
